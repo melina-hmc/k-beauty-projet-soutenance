9a2b7e6c0b8bebafe0b54f2612fd13709a27650f
Mélina Hammaci <melina.hammacii@gmail.com>
2024-05-29 16:52:55 +0200

    Sauvegarde 2 des modifications du backend des products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
+const { default: mongoose } = require("mongoose");
 
 // Create Product
 const createProduct = asyncHandler(async (req, res) => {
@@ -36,8 +37,191 @@ const createProduct = asyncHandler(async (req, res) => {
     })
 
     res.status(201).json(product);
+});
+
+// Get Products
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find().sort("-createdAt")
+    res.status(200).json(products);
+
+});
+
+// Get Single Product
+const getProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+        res.status(404);
+        throw new Error("Product not found ")
+    }
+    res.status(200).json(product);
+    // Pour le tester dans Thunder Client il  faudra copier collé dans la barre un id d'un produit après products/, par exemple: localhost:9001/api/products/66572aed17bccd5d4fe48fd5
+});
+
+// delete product
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+        res.status(404);
+        throw new Error("Product not found ")
+    }
+    await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json({message : "Product Deleted !"});
 })
 
-module.exports = {
+// update product
+
+const updateProduct = asyncHandler(async (req, res) => {
+    const { 
+        name,
+        category,
+        brand,
+        quantity,
+        description,
+        image,
+        regularPrice,
+        price,
+        color, 
+    } = req.body;
+
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+        res.status(404);
+        throw new Error("Product not found ")
+    }
+
+    // Now, update the product
+    const updatedProduct = await Product.findByIdAndUpdate(
+        { _id: req.params.id},
+        {
+            name,
+            category,
+            brand,
+            quantity,
+            description,
+            image,
+            regularPrice,
+            price,
+            color, 
+        },
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+    res.status(200).json(updatedProduct);
+});
+
+// Review product
+
+const reviewProduct = asyncHandler(async (req, res) => {
+    // star, review
+    const { star, review, reviewDate } = req.body;
+    const { id } = req.params;
+  
+    // validation
+    if (star < 1 || !review) {
+      res.status(400);
+      throw new Error("Please add star and review");
+    }
+  
+    const product = await Product.findById(id);
+  
+    // if product doesnt exist
+    if (!product) {
+      res.status(400);
+      throw new Error("Product not found");
+    }
+    if (!product) {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  
+    // Update rating
+    product.ratings.push({
+      star,
+      review,
+      reviewDate,
+      name: req.user.name,
+      userID: req.user._id,
+    });
+    product.save();
+  
+    res.status(200).json({ message: "Product review added" });
+  });
+  
+  // Delete review
+  const deleteReview = asyncHandler(async (req, res) => {
+    const { userID } = req.body;
+  
+    const product = await Product.findById(req.params.id);
+    // if product doesnt exist
+    if (!product) {
+      res.status(400);
+      throw new Error("Product not found");
+    }
+  
+    const newRatings = product.ratings.filter((rating) => {
+      return rating.userID.toString() !== userID.toString();
+    });
+    product.ratings = newRatings;
+    product.save();
+    res.status(200).json({ message: "Product review deleted" });
+  });
+  
+  // Update Review
+  const updateReview = asyncHandler(async (req, res) => {
+    const { star, review, reviewDate, userID } = req.body;
+    const { id } = req.params;
+  
+    // validation
+    if (star < 1 || !review) {
+      res.status(400);
+      throw new Error("Please add star and review");
+    }
+  
+    const product = await Product.findById(id);
+  
+    // if product doesnt exist
+    if (!product) {
+      res.status(400);
+      throw new Error("Product not found");
+    }
+    // Match user to review
+    if (req.user._id.toString() !== userID) {
+      res.status(401);
+      throw new Error("User not authorized");
+    }
+  
+    // Update Product review
+    const updatedReview = await Product.findOneAndUpdate(
+      { _id: product._id,
+        "ratings.userID": mongoose.Types.ObjectId(userID)
+      },
+      {
+        $set: {
+          "ratings.$.star": Number(star),
+          "ratings.$.review": review,
+          "ratings.$.reviewDate": reviewDate,
+        },
+      }
+    );
+  
+    if (updatedReview) {
+      res.status(200).json({ message: "Product review updated" });
+    } else {
+      res.status(400).json({ message: "Product review NOT updated" });
+    }
+  });
+  
+  module.exports = {
     createProduct,
-};
+    getProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct,
+    reviewProduct,
+    deleteReview, 
+    updateReview,
+  };
+  
